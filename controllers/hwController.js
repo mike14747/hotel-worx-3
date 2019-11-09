d905185ab2932a93f3df8b1b0422c5c52d73f854
Sibel <sbaslamisli@yahoo.com>
2019-11-09 13:30:21 -0500

    calendar component created

--- controllers/hwController.js
+++ controllers/hwController.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-const db = require('../../models/index.js');
+const db = require('../models/index.js');
 
 // '/api/hw' route
 router.route('/').get((req, res) => {
