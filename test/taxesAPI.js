193d79e9207e23524ca77bbb5eeb5a00264fd1f3
mike14747 <mike4747@oh.rr.com>
2020-07-05 20:12:06 -0400

    Added a chargesAPI.js test file. Converted all mocha test functions from arrow syntax to old style functions because of the way Mocha uses 'this'.

--- test/taxesAPI.js
+++ test/taxesAPI.js
@@ -5,16 +5,16 @@ const server = require('../server');
 chai.should();
 chai.use(chaiHttp);
 
-describe('Taxes API', () => {
-    describe('GET /api/taxes', () => {
-        it('should get all the taxes and their ids/names/rates/active', (done) => {
+describe('Taxes API', function () {
+    describe('GET /api/taxes', function () {
+        it('should get all the taxes and their ids/names/rates/active', function (done) {
             chai.request(server)
                 .get('/api/taxes')
-                .end((error, response) => {
+                .end(function (error, response) {
                     if (error) console.log(error);
                     response.should.have.status(200);
                     response.body.should.be.an('array').and.have.lengthOf.at.least(1);
-                    response.body.forEach(element => {
+                    response.body.forEach(function (element) {
                         element.should.have.all.keys('tax_id', 'tax_name', 'tax_rate', 'active');
                         element.tax_id.should.be.a('number');
                         element.tax_name.should.be.a('string');
@@ -26,11 +26,11 @@ describe('Taxes API', () => {
         });
     });
 
-    describe('GET /api/taxes/1', () => {
-        it('should get a single tax by id and its id/names/ratesactive', (done) => {
+    describe('GET /api/taxes/1', function () {
+        it('should get a single tax by id and its id/names/ratesactive', function (done) {
             chai.request(server)
                 .get('/api/taxes/1')
-                .end((error, response) => {
+                .end(function (error, response) {
                     if (error) console.log(error);
                     response.should.have.status(200);
                     response.body.should.be.an('array').and.have.lengthOf(1);
@@ -44,11 +44,11 @@ describe('Taxes API', () => {
         });
     });
 
-    describe('GET /api/taxes/9999', () => {
-        it('should return an empty array since 9999 should not match any tax ids', (done) => {
+    describe('GET /api/taxes/0', function () {
+        it('should return an empty array since 0 should not match any tax ids', function (done) {
             chai.request(server)
-                .get('/api/taxes/9999')
-                .end((error, response) => {
+                .get('/api/taxes/0')
+                .end(function (error, response) {
                     if (error) console.log(error);
                     response.should.have.status(200);
                     response.body.should.be.a('array').that.is.empty;
@@ -57,11 +57,11 @@ describe('Taxes API', () => {
         });
     });
 
-    describe('GET /api/taxes/1a', () => {
-        it('should get a status 404 because the tax id param is not an integer', (done) => {
+    describe('GET /api/taxes/1a', function () {
+        it('should get a status 404 because the tax id param is not an integer', function (done) {
             chai.request(server)
                 .get('/api/taxes/1a')
-                .end((error, response) => {
+                .end(function (error, response) {
                     if (error) console.log(error);
                     response.should.have.status(404);
                     done();
