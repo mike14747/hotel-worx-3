a68c5bdb8467af9571f582ffac3e1455f2f57140
mike14747 <mike4747@oh.rr.com>
2020-07-19 11:20:35 -0400

    Added POST and PUT methods to the accessLevel model.

--- models/accessLevel.js
+++ models/accessLevel.js
@@ -24,10 +24,31 @@ const AccessLevel = {
         }
     },
     addNewAccessLevel: async (paramsObj) => {
-
+        try {
+            const queryString = 'INSERT INTO access_levels (access_level, access_type) VALUES(?, ?);';
+            const queryParams = [
+                paramsObj.access_level,
+                paramsObj.access_type,
+            ];
+            const [result] = await pool.query(queryString, queryParams);
+            return [result, null];
+        } catch (error) {
+            return [null, error];
+        }
     },
     updateAccessLevel: async (paramsObj) => {
-
+        try {
+            const queryString = 'UPDATE access_levels SET access_level=?, access_type=? WHERE access_id=?;';
+            const queryParams = [
+                paramsObj.access_level,
+                paramsObj.access_type,
+                paramsObj.access_id,
+            ];
+            const [result] = await pool.query(queryString, queryParams);
+            return [result, null];
+        } catch (error) {
+            return [null, error];
+        }
     },
     deleteAccessLevel: async (paramsObj) => {
         try {
