fbcea46cc1b52846ea378a3c7a1c2b7aa95c907c
mike14747 <mike4747@oh.rr.com>
2020-07-25 18:41:09 -0400

    Changed the it functions in the checkRoutesReady.js test to use the done callback instead of async/await for now... until I have a better understanding of how to handle the returned promise rejections in chai-http.

--- test/checkRoutesReady.js
+++ test/checkRoutesReady.js
@@ -26,18 +26,27 @@ describe('Test authenticated routes', function () {
         require('./tests/cleanup');
     };
 
-    it('should login a user, via POST', async function () {
-        const response = await agent.post('/api/auth/login').send(userCredentials);
-        response.should.have.status(200);
-        response.should.be.json;
-        response.body.should.have.property('user').and.to.be.an('object');
+    it('should login a user, via POST', function (done) {
+        agent
+            .post('/api/auth/login')
+            .send(userCredentials)
+            .end(function (error, response) {
+                response.should.have.status(200);
+                response.should.be.json;
+                response.body.should.have.property('user').and.to.be.an('object');
+                done();
+            });
     });
 
-    it('should check and see if the user is logged in', async function () {
-        const response = await agent.get('/api/auth/status');
-        response.should.have.status(200);
-        response.should.be.json;
-        response.body.should.have.property('user').and.to.be.an('object');
-        if (response.status === 200) runTests();
+    it('should check and see if the user is logged in', function (done) {
+        agent
+            .get('/api/auth/status')
+            .end(function (error, response) {
+                response.should.have.status(200);
+                response.should.be.json;
+                response.body.should.have.property('user').and.to.be.an('object');
+                if (response.status === 200) runTests();
+                done();
+            });
     });
 });
