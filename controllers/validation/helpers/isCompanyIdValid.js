057a1b030eeccac287e97f8014e527b26454c35c
mike14747 <mike4747@oh.rr.com>
2020-08-25 11:55:32 -0400

    Added a lot of new validation helpers (to check for valid ids). The first half of them are complete.

---
+++ controllers/validation/helpers/isCompanyIdValid.js
@@ -0,0 +1,9 @@
+const Company = require('../../../models/company');
+
+const isCompanyIdValid = async (id) => {
+    const [data, error] = await Company.getCompanyById({ id });
+    if (error) throw new Error(error);
+    if (data && data.length !== 1) throw new RangeError('company_id does not exist!');
+};
+
+module.exports = isCompanyIdValid;
