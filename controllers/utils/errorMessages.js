3d892323b828134bcc2ba5db0b2ee713cc8183bd
mike14747 <mike4747@oh.rr.com>
2020-08-28 17:28:54 -0400

    Using a new validation in taxesSchema.js for the active boolean.

--- controllers/utils/errorMessages.js
+++ controllers/utils/errorMessages.js
@@ -1,9 +1,9 @@
 const postError = 'An error occurred trying to add the new record.';
 const putError = 'An error occurred trying to update the record.';
 const deleteError = 'An error occurred trying to delete the record.';
-const activeError = 'The active field serves as a boolean and must be either 0 or 1.';
-const taxableError = 'The taxable field serves as a boolean and must be either 0 or 1.';
-const taxExemptError = 'The tax_exempt field serves as a boolean and must be either 0 or 1.';
+const activeError = 'The "active" field serves as a boolean and must be either 0 or 1.';
+const taxableError = 'The "taxable" field serves as a boolean and must be either 0 or 1.';
+const taxExemptError = 'The "tax_exempt" field serves as a boolean and must be either 0 or 1.';
 
 module.exports = {
     postError,
