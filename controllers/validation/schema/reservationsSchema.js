12c62662f4f6a349b55d6a557a41ceecb51b3bad
“mike14747” <mike4747@oh.rr.com>
2020-09-03 23:04:46 -0400

    Started on reservationsSchema.js and updating reservationsController.js to accommodate it.

---
+++ controllers/validation/schema/reservationsSchema.js
@@ -0,0 +1,30 @@
+const Joi = require('joi');
+const { activeError } = require('../../utils/errorMessages');
+
+const reservationNewSchema = Joi.object({
+
+});
+
+const reservationUpdateSchema = Joi.object({
+    reservation_id: Joi.number().integer().min(0).required(),
+    customer_id: Joi.number().integer().min(0).required(),
+    user_id: Joi.number().integer().min(0).required(),
+    company_id: Joi.number().integer().min(0),
+    comments: Joi.string().required(),
+    active: Joi.number().integer().min(0).max(1).messages({
+        'number.base': activeError,
+        'number.integer': activeError,
+        'number.min': activeError,
+        'number.max': activeError,
+    }).required(),
+});
+
+const reservationIdSchema = Joi.object({
+    reservation_id: Joi.number().integer().min(0).required(),
+});
+
+module.exports = {
+    reservationNewSchema,
+    reservationUpdateSchema,
+    reservationIdSchema,
+};
