008d6b7338abcda51326c24f40e60cda2bfd524f
Sibel <sbaslamisli@yahoo.com>
2020-10-20 16:30:34 -0400

    splitscreen setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,72 +1,81 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
-import Calendar from './components/calendar/index';
-import NavBar from './components/navbar/navbar';
-import Generic from './pages/generic/generic';
-import AxiosTest from './pages/axiosTest/axiosTest';
-import UserContext from './context/userContext';
-import Loading from './components/loading/loading';
-import axios from 'axios';
-import ProtectedRoute from './components/protectedRoute/protectedRoute';
-import NoMatch from './pages/noMatch/noMatch';
-import Login from './pages/login/login';
-import './css/my_style.css';
-import './css/styles.css';
+import React, { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import Calendar from "./components/calendar/index";
+import NavBar from "./components/navbar/navbar";
+import Generic from "./pages/generic/generic";
+import AxiosTest from "./pages/axiosTest/axiosTest";
+import UserContext from "./context/userContext";
+import Loading from "./components/loading/loading";
+import axios from "axios";
+import ProtectedRoute from "./components/protectedRoute/protectedRoute";
+import NoMatch from "./pages/noMatch/noMatch";
+import Login from "./pages/login/login";
+import Home from "./pages/Home/home";
+import "./css/my_style.css";
+import "./css/styles.css";
 
 export default function App() {
-    const [user, setUser] = useState(null);
-    const [hasStatusLoaded, setHasStatusLoaded] = useState(false);
+  const [user, setUser] = useState(null);
+  const [hasStatusLoaded, setHasStatusLoaded] = useState(false);
 
-    useEffect(() => {
-        axios.get('/api/auth/status')
-            .then(response => {
-                setUser(response.data.user);
-            })
-            .catch((error) => {
-                console.log(error);
-                setUser(null);
-            })
-            .finally(() => setHasStatusLoaded(true));
-    }, []);
+  useEffect(() => {
+    axios
+      .get("/api/auth/status")
+      .then((response) => {
+        setUser(response.data.user);
+      })
+      .catch((error) => {
+        console.log(error);
+        setUser(null);
+      })
+      .finally(() => setHasStatusLoaded(true));
+  }, []);
 
-    if (!hasStatusLoaded) {
-        return (
-            <div className="m-5">
-                <Loading />
-            </div>
-        );
-    }
-
+  if (!hasStatusLoaded) {
     return (
-        <Router>
-            <UserContext.Provider value={{ user, setUser }}>
-                <div className="container py-4 flex-fill bg-white border border-secondary">
-                    <NavBar />
-                    <Switch>
-                        <Route exact path="/about">
-                            <About />
-                        </Route>
-                        <Route exact path="/">
-                            <Home />
-                        </Route>
-                        <Route exact path="/generic" component={Generic} />
-                        <Route exact path="/calendar" component={Calendar} />
-                        <ProtectedRoute exact path="/axiostest" user={user} component={AxiosTest} />
-                        <Route exact path="/login">
-                            {user ? <Redirect to="/" /> : <Login />}
-                        </Route>
-                        <Route component={NoMatch} />
-                    </Switch>
-                </div>
-            </UserContext.Provider>
-        </Router>
+      <div className="m-5">
+        <Loading />
+      </div>
     );
-}
+  }
 
-function Home() {
-    return <h2>Home</h2>;
+  return (
+    <Router>
+      <UserContext.Provider value={{ user, setUser }}>
+        <div className="container py-4 flex-fill bg-white border border-secondary">
+          <NavBar />
+          <Switch>
+            <Route exact path="/about">
+              <About />
+            </Route>{" "}
+            <Route exact path="/">
+              <Home />
+            </Route>{" "}
+            <Route exact path="/generic" component={Generic} />{" "}
+            <Route exact path="/calendar" component={Calendar} />{" "}
+            <ProtectedRoute
+              exact
+              path="/axiostest"
+              user={user}
+              component={AxiosTest}
+            />{" "}
+            <Route exact path="/login">
+              {" "}
+              {user ? <Redirect to="/" /> : <Login />}{" "}
+            </Route>{" "}
+            <Route component={NoMatch} />{" "}
+          </Switch>{" "}
+        </div>{" "}
+      </UserContext.Provider>{" "}
+    </Router>
+  );
 }
 
 function About() {
-    return <h2>About</h2>;
+  return <h2> About </h2>;
 }
---
+++ client/src/components/SplitScreen/SplitScreen.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class splitScreen extends Component {
+    render() {
+        return (
+            <div>
+                <h2>This is a split screen</h2>
+            </div>
+        )
+    }
+}
