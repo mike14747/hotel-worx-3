548cdc764e87b67e7dfcc633b05b772f9aab7da6
Sibel <sbaslamisli@yahoo.com>
2020-10-19 22:01:32 -0400

    added pages

--- client/src/components/calendar/index.js
+++ client/src/components/calendar/index.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import './calendar.css';
-const startDateMin = new Date().toISOString().split('T')[0];
+import React, { useState } from "react";
+import ''./calendar.css';
+const startDateMin = new Date().toISOString().split("T")[0];
 
 const Calendar = () => {
     const [state, setState] = useState({
@@ -8,33 +8,34 @@ const Calendar = () => {
         endDate: '',
     });
 
-    const handleChange = (e) => {
-        setState({
-            [e.target.name]: e.target.value,
-        });
-    };
+  const handleChange = (e) => {
+    setState({
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    return (
-        <div>
-            <h1>Calendar</h1>
-            <h3>Arrival Date</h3>
-            <input type="date"
-                id="startDate"
-                name="startDate"
-                min={startDateMin}
-                className="date-picker"
-                onChange={handleChange}
-            />
-            <h3>Departure Date</h3>
-            <input type="date"
-                id="endDate"
-                name="endDate"
-                min={state.startDate}
-                className="date-picker"
-                onChange={handleChange}
-            />
-        </div >
-    );
+  return (
+    <div>
+      <h1> Calendar </h1> <h3> Arrival Date </h3>{" "}
+      <input
+        type="date"
+        id="startDate"
+        name="startDate"
+        min={startDateMin}
+        className="date-picker"
+        onChange={handleChange}
+      />{" "}
+      <h3> Departure Date </h3>{" "}
+      <input
+        type="date"
+        id="endDate"
+        name="endDate"
+        min={state.startDate}
+        className="date-picker"
+        onChange={handleChange}
+      />{" "}
+    </div>
+  );
 };
 
 export default Calendar;
