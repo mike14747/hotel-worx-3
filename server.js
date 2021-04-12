3dbb1a16f40a2f316ffbef98d8dea46cde27e19d
mike14747 <mike4747@oh.rr.com>
2021-04-12 11:10:42 -0400

    Set server.js to point to /docs/index.html when in production mode so the api routes page is the only thing showing up on heroku.

--- server.js
+++ server.js
@@ -38,10 +38,15 @@ dbTest()
         });
     })
     .finally(() => {
+        // if (process.env.NODE_ENV === 'production') {
+        //     app.use(express.static(path.join(__dirname, 'client/build')));
+        //     app.get('*', (req, res) => {
+        //         res.sendFile(path.join(__dirname, 'client/build/index.html'));
+        //     });
+        // }
         if (process.env.NODE_ENV === 'production') {
-            app.use(express.static(path.join(__dirname, 'client/build')));
             app.get('*', (req, res) => {
-                res.sendFile(path.join(__dirname, 'client/build/index.html'));
+                res.sendFile(path.join(__dirname, 'docs/index.html'));
             });
         }
     });
