622c07f89b277ccf2bcd0a2f3727e057bbb5b085
mike14747 <mike4747@oh.rr.com>
2021-04-12 11:47:33 -0400

    Updated the sessionStore connection to accomodate heroku's jawsdb string.

--- passport/sessionStore.js
+++ passport/sessionStore.js
@@ -3,13 +3,26 @@ const pool = require('../config/connectionPool').getDb();
 const session = require('express-session');
 const MySQLStore = require('express-mysql-session')(session);
 
-const sessionOptions = {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PW,
-    database: process.env.DB_NAME,
-};
+let sessionOptions;
+
+if (process.env.JAWSDB_URL) {
+    const url = new URL(process.env.JAWSDB_URL);
+    sessionOptions = {
+        host: url.hostname,
+        port: url.port,
+        user: url.username,
+        password: url.password,
+        database: url.pathname.replace("/", ""),
+    };
+} else {
+    sessionOptions = {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PW,
+        database: process.env.DB_NAME,
+    };
+}
 
 const sessionStore = new MySQLStore(sessionOptions, pool);
 
