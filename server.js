c7d0248f0520a98580a21e1895ddcd4253855511
mike14747 <mike4747@oh.rr.com>
2021-04-12 12:40:23 -0400

    Commented out most of the code in server.js to see if just a res.send() will work on heroku.

--- server.js
+++ server.js
@@ -9,50 +9,55 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-function checkAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-        return next();
-    } else {
-        // return res.status(401).json({ message: 'User must be logged in to access these routes!' });
-        return next();
-    }
-}
+app.get('*', (req, res) => {
+    // res.sendFile(path.join(__dirname, 'docs/index.html'));
+    res.send('This is a test page!');
+});
 
-const { dbTest } = require('./config/connectionPool');
+// function checkAuthenticated(req, res, next) {
+//     if (req.isAuthenticated()) {
+//         return next();
+//     } else {
+//         // return res.status(401).json({ message: 'User must be logged in to access these routes!' });
+//         return next();
+//     }
+// }
 
-app.use(require('./controllers/testController'));
+// const { dbTest } = require('./config/connectionPool');
 
-dbTest()
-    .then(() => {
-        app.use(require('./passport/expressSession'));
-        const passport = require('./passport/passportFunctions');
-        app.use(passport.initialize());
-        app.use(passport.session());
-        app.use('/api/auth', require('./controllers/authController'));
-        app.use('/api', checkAuthenticated, require('./controllers'));
-    })
-    .catch((error) => {
-        console.log('An error occurred connecting to the database!', error.message);
-        app.get('/api/*', (req, res) => {
-            res.status(500).json({ Error: 'There is no connection to the database!' });
-        });
-    })
-    .finally(() => {
-        // if (process.env.NODE_ENV === 'production') {
-        //     app.use(express.static(path.join(__dirname, 'client/build')));
-        //     app.get('*', (req, res) => {
-        //         res.sendFile(path.join(__dirname, 'client/build/index.html'));
-        //     });
-        // }
+// app.use(require('./controllers/testController'));
 
-        // use this code if you only want the description of the api to appear on the deployed version at heroku
-        if (process.env.NODE_ENV === 'production') {
-            app.get('*', (req, res) => {
-                // res.sendFile(path.join(__dirname, 'docs/index.html'));
-                res.send('This is a test page!');
-            });
-        }
-    });
+// dbTest()
+//     .then(() => {
+//         app.use(require('./passport/expressSession'));
+//         const passport = require('./passport/passportFunctions');
+//         app.use(passport.initialize());
+//         app.use(passport.session());
+//         app.use('/api/auth', require('./controllers/authController'));
+//         app.use('/api', checkAuthenticated, require('./controllers'));
+//     })
+//     .catch((error) => {
+//         console.log('An error occurred connecting to the database!', error.message);
+//         app.get('/api/*', (req, res) => {
+//             res.status(500).json({ Error: 'There is no connection to the database!' });
+//         });
+//     })
+//     .finally(() => {
+//         // if (process.env.NODE_ENV === 'production') {
+//         //     app.use(express.static(path.join(__dirname, 'client/build')));
+//         //     app.get('*', (req, res) => {
+//         //         res.sendFile(path.join(__dirname, 'client/build/index.html'));
+//         //     });
+//         // }
+
+//         // use this code if you only want the description of the api to appear on the deployed version at heroku
+//         if (process.env.NODE_ENV === 'production') {
+//             app.get('*', (req, res) => {
+//                 // res.sendFile(path.join(__dirname, 'docs/index.html'));
+//                 res.send('This is a test page!');
+//             });
+//         }
+//     });
 
 app.listen(PORT, () => console.log('Server is listening on port ' + PORT));
 
